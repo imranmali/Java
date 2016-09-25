24a5875f2bf81fa868502a333e87afc9cad89dd1
imranmali <imranmohammadali@gmail.com>
2016-09-25 15:18:18 +0800

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,5 @@
+
+#!/bin/bash
+#Check memory size : ps -u username -o rss,etime,pid,command
+export JAVA_OPTIONS="-Xms 32m -Xmx256m -XX:MaxPermSize=265m"
+java -jar jetty/jetty-runner-9.2.6.v20141205.jar --port 8080 --log jetty.log target/SpringFileUpload.war
